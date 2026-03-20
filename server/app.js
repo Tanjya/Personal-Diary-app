b023c6c4d5af9eb0cf4dfc8bc9252bb3e1bf0830
Tanjya Akther <tanjya26@gmail.com>
2026-03-20 15:11:10 +0000

    fix links

--- server/app.js
+++ server/app.js
@@ -8,8 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../public/index.html')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/diary', diaryRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = app;
